3080d495651e7b60f0c304bf97ca7a411a71a4f4
srbakoski <srbakoski98@hotmail.com>
2024-03-29 16:32:20 +0100

    getVotes draft logic added

--- packages/hardhat/contracts/SablierGovernor.sol
+++ packages/hardhat/contracts/SablierGovernor.sol
@@ -1,14 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-import "@openzeppelin/contracts/governance/Governor.sol";
-import "@openzeppelin/contracts/governance/extensions/GovernorSettings.sol";
-import "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
-import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
-import "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
-import "@openzeppelin/contracts/governance/extensions/GovernorTimelockControl.sol";
+import { Governor, IGovernor } from "@openzeppelin/contracts/governance/Governor.sol";
+import { GovernorSettings } from "@openzeppelin/contracts/governance/extensions/GovernorSettings.sol";
+import { GovernorCountingSimple } from "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
+import { GovernorVotes, IVotes } from "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
+import { GovernorVotesQuorumFraction } from "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
+import { GovernorTimelockControl, TimelockController } from "@openzeppelin/contracts/governance/extensions/GovernorTimelockControl.sol";
+import { ISablierLinear } from "./interfaces/ISablierLinear.sol";
+import { ISablierDynamic } from "./interfaces/ISablierDynamic.sol";
+import { LockupLinear, LockupDynamic } from "@sablier/v2-core/src/types/DataTypes.sol";
 
-contract MyGovernor is
+contract SablierGovernor is
 	Governor,
 	GovernorSettings,
 	GovernorCountingSimple,
@@ -16,19 +19,35 @@ contract MyGovernor is
 	GovernorVotesQuorumFraction,
 	GovernorTimelockControl
 {
+	enum StreamType {
+		Linear,
+		Dynamic
+	}
+	struct StreamParams {
+		uint256 streamID;
+		StreamType streamType;
+	}
+
+	ISablierLinear public immutable i_sablierLinear;
+	ISablierDynamic public immutable i_sablierDynamic;
+
 	constructor(
 		IVotes _token,
-		TimelockController _timelock
+		TimelockController _timelock,
+		ISablierLinear sablierLinear,
+		ISablierDynamic sablierDynamic
 	)
 		Governor("MyGovernor")
 		GovernorSettings(7200 /* 1 day */, 50400 /* 1 week */, 0)
 		GovernorVotes(_token)
 		GovernorVotesQuorumFraction(4)
 		GovernorTimelockControl(_timelock)
-	{}
+	{
+		i_sablierLinear = sablierLinear;
+		i_sablierDynamic = sablierDynamic;
+	}
 
 	// The following functions are overrides required by Solidity.
-
 	function votingDelay()
 		public
 		view
@@ -110,5 +129,80 @@ contract MyGovernor is
 		bytes32 descriptionHash
 	) internal override(Governor, GovernorTimelockControl) {
 		super._execute(proposalId, targets, values, calldatas, descriptionHash);
+	}
+
+	function _getVotes(
+		address account,
+		uint256 timepoint,
+		bytes memory params
+	)
+		internal
+		view
+		virtual
+		override(Governor, GovernorVotes)
+		returns (uint256)
+	{
+		uint256 regularVotes = super._getVotes(account, timepoint, params);
+		uint256 streamVotes;
+
+		StreamParams[] memory streams = abi.decode(params, (StreamParams[]));
+		if (streams.length == 0) {
+			streamVotes = 0;
+		} else {
+			for (uint256 i = 0; i < streams.length; i++) {
+				uint256 streamId = streams[i].streamID;
+
+				if (streams[i].streamType == StreamType.Linear) {
+					// @dev Reverts if `streamId` references a null stream.
+					LockupLinear.Stream memory stream = i_sablierLinear
+						.getStream(streamId);
+
+					// add if stream was already used for voting
+					if (address(stream.asset) != address(token)) {
+						continue;
+					}
+
+					if (i_sablierLinear.getRecipient(streamId) != account) {
+						revert("Recipient does not match account");
+					}
+
+					if (stream.wasCanceled) {
+						streamVotes += i_sablierLinear.withdrawableAmountOf(
+							streamId
+						);
+					} else {
+						streamVotes +=
+							stream.amounts.deposited -
+							stream.amounts.withdrawn;
+					}
+				} else {
+					// @dev Reverts if `streamId` references a null stream.
+					LockupDynamic.Stream memory stream = i_sablierDynamic
+						.getStream(streamId);
+
+					// add if stream was already used for voting
+					// add last valid stream check
+					if (address(stream.asset) != address(token)) {
+						continue;
+					}
+
+					if (i_sablierDynamic.getRecipient(streamId) != account) {
+						revert("Recipient does not match account");
+					}
+
+					if (stream.wasCanceled) {
+						streamVotes += i_sablierDynamic.withdrawableAmountOf(
+							streamId
+						);
+					} else {
+						streamVotes +=
+							stream.amounts.deposited -
+							stream.amounts.withdrawn;
+					}
+				}
+			}
+		}
+
+		return regularVotes + streamVotes;
 	}
 }
---
+++ packages/hardhat/contracts/interfaces/ISablierDynamic.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import { LockupDynamic } from "@sablier/v2-core/src/types/DataTypes.sol";
+
+/// @title ISablierDynamic
+interface ISablierDynamic {
+	/// @notice Retrieves the stream's recipient.
+	/// @dev Reverts if the NFT has been burned.
+	/// @param streamId The stream id for the query.
+	function getRecipient(
+		uint256 streamId
+	) external view returns (address recipient);
+
+	/// @notice Retrieves the stream entity.
+	/// @dev Reverts if `streamId` references a null stream.
+	/// @param streamId The stream id for the query.
+	function getStream(
+		uint256 streamId
+	) external view returns (LockupDynamic.Stream memory stream);
+
+	/// @notice Calculates the amount that the recipient can withdraw from the stream, denoted in units of the asset's
+	/// decimals.
+	/// @dev Reverts if `streamId` references a null stream.
+	/// @param streamId The stream id for the query.
+	function withdrawableAmountOf(
+		uint256 streamId
+	) external view returns (uint128 withdrawableAmount);
+}
