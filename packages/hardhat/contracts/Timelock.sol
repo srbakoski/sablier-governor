ff368bbe75ffec1d35be11adf3a9040f899792a1
srbakoski <srbakoski98@hotmail.com>
2024-03-29 14:18:17 +0100

    draft contracts added

---
+++ packages/hardhat/contracts/Timelock.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import "@openzeppelin/contracts/governance/TimelockController.sol";
+
+contract Timelock is TimelockController {
+	// Admin is set to address(0) so there is no account with privileges
+	constructor(
+		uint256 minDelay,
+		address[] memory proposers,
+		address[] memory executors
+	) TimelockController(minDelay, proposers, executors, address(0)) {}
+}
